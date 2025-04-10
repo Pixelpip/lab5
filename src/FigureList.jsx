e8bd165fb46979593bd3402a91e753898163ad5d
Pixelpip <athulkamathg5@gmail.com>
2025-04-11 00:23:03 +0530

    msg

--- src/FigureList.jsx
+++ src/FigureList.jsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
 import BasicFigure from "./BasicFigure";
 import "./FigureList.css"; // Import CSS for styling
-const rand1=Math.floor(Math.random()*1000)
-const rand2=Math.floor(Math.random()*1000)
-const rand3=Math.floor(Math.random()*1000)
+const rand=Math.floor(Math.random()*1000)
 
 const initialImages = [
-  { src: `https://picsum.photos/200/400?random=${rand1}`, caption: "Image 1" },
-  { src: `https://picsum.photos/200/400?random=${rand2}`, caption: "Image 2" },
-  { src: `https://picsum.photos/200/400?random=${rand3}`, caption: "Image 3" }
+  { src: `https://picsum.photos/200/400?random=${rand}`, caption: "Image 1" },
+  { src: `https://picsum.photos/200/400?random=${rand+1}`, caption: "Image 2" },
+  { src: `https://picsum.photos/200/400?random=${rand+2}`, caption: "Image 3" }
 ];
 const FigureList = () => {
   const [images, setImages] = useState(initialImages);
   const addImage = () => {
     const randomId = Math.floor(Math.random() * 1000);
     const newImage = {
-      src: `https://picsum.photos/400/300?random=${randomId}`,
+      src: `https://picsum.photos/200/400?random=${randomId}`,
       caption: `Image ${images.length + 1}`
     };
     setImages([...images, newImage]);
